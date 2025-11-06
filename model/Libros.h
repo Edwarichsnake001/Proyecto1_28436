da073f30c7957744a9fb7ab6dd8e45e91ac33c1a
Edwarichsnake001 <materoge32@gmail.com>
2025-11-06 18:21:50 -0500

    GenericoLibro

--- model/Libros.h
+++ model/Libros.h
@@ -1,5 +1,38 @@
 #include "Lista.h"
+#include <iostream>
+#include <ostream>
 #include <string>
 
 using namespace std;
 
+class Libros
+{
+private:
+    string titulo;
+    string autor;
+    string codigo;
+    bool disponible;
+
+public:
+    Libros(string tit, string aut, string cod, bool disp) : titulo(tit), autor(aut), codigo(cod), disponible(disp){}
+
+    string getTitulo()const {
+        return titulo;
+    }
+
+    string getAutor()const {
+        return autor;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Libros& libro) {
+        os << "Título: " << libro.titulo
+           << ", Autor: " << libro.autor
+           << ", Código: " << libro.codigo
+           << ", Estatus: " << (libro.disponible ? "Disponible" : "No disponible");
+        return os;
+    }
+
+
+
+
+};
