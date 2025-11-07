2e4016369041ceaeb5ce78086ea1187937cb9d34
Edwarichsnake001 <materoge32@gmail.com>
2025-11-07 16:12:20 -0500

    Cola completa

--- model/Cola.h
+++ model/Cola.h
@@ -13,8 +13,61 @@ private:
 public:
     Cola(): front(NULL), rear(NULL){}
 
-    
-
-
+    void encolar(const T& dato);
+    T desencolar();
+    bool estaVacia() const;
+    void mostrar() const;
 
 };
+
+template <typename T>
+inline bool Cola<T>::estaVacia() const
+{
+    return front == NULL;
+}
+
+template <typename T>
+ void Cola<T>::mostrar() const
+{
+    NodoCola<T>* actual = front;
+    while (actual != nullptr) {
+        std::cout << actual->getDato() << std::endl;
+        actual = actual->getSiguiente();
+    }
+}
+
+
+template <typename T>
+void Cola<T>::encolar(const T &dato)
+{
+    NodoCola<T> *nuevo = new NodoCola<T>(dato);
+    if(estaVacia()){
+        front = rear = nuevo;
+    }else{
+        rear->setSiguiente(nuevo)
+        rear = nuevo;
+    }
+}
+
+template <typename T>
+T Cola<T>::desencolar()
+{
+    if (estaVacia())
+    {
+        cout << "La cola está vacía\n";
+        return T();
+    }
+
+    NodoCola<T> *temp= front;
+    T dato = temp->getDato();
+    front = front -> getSiguiente();
+
+    if (front == NULL)
+    {
+        rear = NULL;
+    }
+
+    delete temp;
+    return dato;    
+    return T();
+}
